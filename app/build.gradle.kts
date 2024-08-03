e8749e6b6a29aa8275b19045152cde526dc0bc94
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-08-03 20:26:41 +0900

    마지막 위치 가져오기 - 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
