64f77556e42b4ccf3f3adeb48f67204d17965f72
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-08-03 13:53:04 +0900

    기상청 단기예보 조회 API 호출

--- app/src/main/java/fastcampus/part2/weatherapp/MainActivity.kt
+++ app/src/main/java/fastcampus/part2/weatherapp/MainActivity.kt
@@ -1,16 +1,54 @@
 package fastcampus.part2.weatherapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://apis.data.go.kr/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(WeatherService::class.java)
+
+        service.getVillageForecast(
+            serviceKey = "fbK2g297uMEM8V6tRh8OrEcJYGYvS2aK/hLSkVSySexCD0yEVarZgDG7Li6ZbrOy1Wa++Irb+dZHjwnpnSDHBA==",
+            baseDate = "20240803",
+            baseTime = "1400",
+            nx = 55,
+            ny = 127
+        ).enqueue(object: Callback<WeatherEntity>{
+            override fun onResponse(call: Call<WeatherEntity>, response: Response<WeatherEntity>) {
+
+                // todo if(response.isSuccessful) 예외처리
+
+                val forecastList = response.body()?.response?.body?.items?.forecastEntities.orEmpty()
+
+                for(forecast in forecastList){
+                    Log.e("Forecast", forecast.toString())
+                }
+
+            }
+
+            override fun onFailure(call: Call<WeatherEntity>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
 
     }
 }
